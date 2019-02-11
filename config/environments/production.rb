4dbce6c1b9bba9560e505543e04e1324beb8bf73
Aleksandr Khomakha <alexfcdp@gmail.com>
2019-02-11 22:43:58 +0200

    fixed default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Rails.application.routes.default_url_options[:host] = 'https://alexfcdp_todo_list.herokuapp.com/'
+  Rails.application.routes.default_url_options[:host] = 'https://alexfcdp-todo-list.herokuapp.com/'
 end
